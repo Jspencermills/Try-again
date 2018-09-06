68f88018637183465dc40680cd1bb66fb92ae80f
Jspencermills <42650418+Jspencermills@users.noreply.github.com>
2018-09-06 09:02:43 -0700

    Update JSpencerPikachu.java

--- JSpencerPikachu.java
+++ JSpencerPikachu.java
@@ -1,6 +1,9 @@
-Public static void main Pikachu(){
+Public class Pikachu {
+  {
+  Public static void main Pikachu()
 System.out.println\t("Pikachu welcomes you to the world of Pokemon\!"\n);
-\t\t\tprintln("(\\__/)"\n);
-\t\t\tprintln("(o^.^)")\n);
-\t\t  ("z(_(\")(\")\n);
-}
+System.out.println\t\t\tprintln("(\\__/)"\n);
+System.out.println\t\t\tprintln("(o^.^)")\n);
+System.out.println\t\t  ("z(_(\")(\")\n);
+    }
+ }
